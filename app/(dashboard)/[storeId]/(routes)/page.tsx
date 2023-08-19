b07921f3840253c9571593d24f268fb1e9398358
Amotekun <97878695+Amotekun@users.noreply.github.com>
2023-08-19 10:11:36 -0700

    error fix

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,4 @@
 import Heading from '@/components/ui/heading'
-import prismadb from '@/lib/prismadb'
 import { Separator } from '@/components/ui/separator'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { CreditCard, DollarSign, Package } from 'lucide-react'
